7ce8c422c13726278213fe982dc49173ac41cd65
Gia Dat Truong <giadattruong@192.168.2.3>
2024-03-04 23:59:18 +0700

    more git aliases

--- aliases.zsh
+++ aliases.zsh
@@ -5,11 +5,12 @@ alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 
 # Git
 alias gst="git status"
-alias gst="git status"
 alias gb="git branch"
 alias gc="git checkout"
+alias gcn="git checkout -b"
 alias pull="git pull"
 alias push="git push"
+alias fetch="git fetch --all"
 alias force="git push --force"
 alias amend="git add . && git commit --amend --no-edit"
 alias commit="git add . && git commit -m"
